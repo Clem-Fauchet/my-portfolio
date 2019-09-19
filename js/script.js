57a068e552d9ed978cab8709ff9e866a4011d2eb
Clem Fauchet <cfauchet.pro@gmail.com>
2019-09-19 19:35:00 +0200

    Essai hover navigation js - Echec.

---
+++ js/script.js
@@ -0,0 +1,25 @@
+$(document).ready(function () {
+
+	var windowWidth = $(window).width();
+	
+	/************* ARROWS ANIMATION ***************/
+	
+	var svg = document.getElementById("arrow_nav");
+	
+	var gS= document.getElementsByClassName('square');
+	
+	var gT= document.getElementsByClassName('triangle');
+	
+	
+	$('.page_nav').mouseover(function(){
+		gT.style.stroke = 'red'
+		
+
+
+	
+	
+				  
+					  
+});
+	
+})
