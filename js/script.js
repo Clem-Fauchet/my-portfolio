605b439ad4b51fe8dd50d5fd6af613348287948a
Clem Fauchet <cfauchet.pro@gmail.com>
2019-09-24 21:25:54 +0200

    Ajout fichier js classie + reste script js.

--- js/script.js
+++ js/script.js
@@ -1,21 +1,8 @@
-$(document).ready(function () {
+$(document.body).ready(function () {
 
 	var windowWidth = $(window).width();
 
 	/************* PAGE LAYERS ***************/
-	var classie = {
-		// full names
-		hasClass: hasClass,
-		addClass: addClass,
-		removeClass: removeClass,
-		toggleClass: toggleClass,
-		// short names
-		has: hasClass,
-		add: addClass,
-		remove: removeClass,
-		toggle: toggleClass
-	};
-
 
 
 	var pages = [].slice.call(document.querySelectorAll('.pages > .page')),
@@ -39,14 +26,56 @@ $(document).ready(function () {
 				nextPage.className = 'page';
 			}
 		};
-	
+
 	revealer = new Revealer(revealerOpts);
 
+	// clicking the page nav buttons
+	document.querySelector('button.button_left').addEventListener('click', function () {
+		reveal('left');
+	});
+	document.querySelector('button.button_right').addEventListener('click', function () {
+		reveal('right');
+	});
+	document.querySelector('button.button_bottom').addEventListener('click', function () {
+		reveal('bottom');
+	});
+
+	// triggers the effect by calling instance.reveal(direction, callbackTime, callbackFn)
+	function reveal(direction) {
+		var callbackTime = 750,
+			callbackFn = function () {
+				classie.remove(pages[currentPage], 'page-current');
+				currentPage = currentPage === 0 ? 1 : 0;
+				classie.add(pages[currentPage], 'page-current');
+			};
+
+		revealer.reveal(direction, callbackTime, callbackFn);
+	}
 
 
+	function changeEffect() {
+		revealer.destroy();
+		var revealerOpts = {
+			// the layers are the elements that move from the sides
+			nmbLayers: Number(this.options[this.selectedIndex].getAttribute('data-layers')),
+			// bg color of each layer
+			bgcolor: this.options[this.selectedIndex].getAttribute('data-colors').split(','),
+			// effect classname
+			effect: 'anim-' + this.value,
+			onStart: function (direction) {
+				// next page gets class page-animate-[direction]
+				var nextPage = pages[currentPage === 0 ? 1 : 0];
+				classie.add(nextPage, 'page-animate-' + direction);
+			},
+			onEnd: function (direction) {
+				// remove class page-animate-[direction] from next page
+				var nextPage = pages[currentPage === 0 ? 1 : 0];
+				nextPage.className = 'page';
+			}
+		};
 
-
-
+		revealer = new Revealer(revealerOpts);
+	}
 
 
 })
